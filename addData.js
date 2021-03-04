b105e44259c15438dd0b0e3bee9ab3becb9f92e5
adelinaiuhas <adelina.iuhas@gmail.com>
2021-03-04 19:34:01 +0200

    clean start

---
+++ addData.js
@@ -0,0 +1,35 @@
+console.log("JavaScript - AJAX"); // Javascript Object Notation
+console.log("Crud Operations - Create");
+console.log("Create Read Update Delete - CRUD");
+
+document
+.getElementById("add-article-button")
+.addEventListener('click', function(){
+
+    
+    const articleTitle = document.getElementById("article-title").value;
+    console.log("articleTitle", articleTitle);
+    const articleContent = document.getElementById("article-content").value;
+    console.log("articleContent", articleContent);
+    if (articleTitle && articleContent) {
+        const payload = {
+            title: articleTitle,
+            content: articleContent,
+        }
+    
+        console.log("Payload:", payload);
+        console.log("Payload Text:", JSON.stringify(payload));
+    
+    
+        fetch("https://simple-json-server-scit.herokuapp.com/posts", {
+            method: "POST",
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload), // body data type must match "Content-Type" header
+          }).then(getData);  
+    }
+});
